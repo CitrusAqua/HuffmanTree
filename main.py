59fe18ce75333e15b67682fcf0ef09332a10fa39
mixplugs <mix.plugs@gmail.com>
2017-10-21 21:03:35 +0800

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import HuffmanTree as ht
-
-my_tree = ht.HuffmanTree.form("inputfile.txt")
-my_tree.output("table.txt")
-my_tree.encode("inputfile.txt", "encoded.txt")
-my_tree.decode("encoded.txt", "decoded.txt")
